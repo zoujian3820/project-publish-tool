73fe4f419b5a9b3581ca6b27d7a38bce88fc0a84
qingyun <qingyun@i9i8.com>
2022-08-23 10:58:55 +0800

    fix: 添加其他系统的预览

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MC方便B端系统发版的快捷小工具
-// @version      0.3.9
+// @version      0.3.10
 // @description  支持批量打开 blushMark商城和uwp和suwp子系统的jenkins与gitlab, 也支持空格批量打开多个单系统dap scm...的jenkins/gitlab
 // @author       mrzou
 // @match        https://bl-sc-pms-t-1.digi800.com/#/index
@@ -25,11 +25,11 @@
 // ==/UserScript==
 
 ;(function () {
-  "use strict"
+  'use strict'
   setTimeout(function () {
     $(function () {
-      function getHtml() {
-        var isHide = localStorage.getItem("monkey-plugin-hide")
+      function getHtml () {
+        var isHide = localStorage.getItem('monkey-plugin-hide')
         var style = `
               <style>
                   .monkey-plugin-warp{
@@ -72,11 +72,11 @@
               `
         return (
           style +
-          `<div class="monkey-plugin-warp ${isHide ? "close" : ""}">
+          `<div class="monkey-plugin-warp ${isHide ? 'close' : ''}">
           <span class="monkey-plugin-close ${
-            isHide ? "monkey-plugin-entry" : ""
+            isHide ? 'monkey-plugin-entry' : ''
           }">
-          ${isHide ? "o" : "X"}  
+          ${isHide ? 'o' : 'X'}  
           </span>
           <i class="monkey-plugin-tips">
             suwp-a/uwp-a为子系统批量操作<br/>支持空格批量<br/>dap scm pc m rv riven-m...
@@ -87,12 +87,15 @@
           </i>
           <input class="monkey-plugin-input" value="rv" placeholder="suwp-a/uwp-a(默认)/单(多)个系统名" />
           <ul>
+          
+            <button val="dev">开发jenkins</button>
             <button val="test">测试jenkins</button>
             <button val="pre">预发布jenkins</button>
             <button val="prod">正式jenkins</button>
             <button val="gitlab">gitlab发master</button>
 
-            <p>商城线上预览地址</p>
+            <p>线上预览地址</p>
+            <span><button val="kfpage">开发</button><input value="1" placeholder="env id"/></span>
             <span><button val="cspage">测试</button><input value="1" placeholder="env id"/></span>
             <span><button val="yfbpage">预发布</button><input value="1" placeholder="env id"/></span>
             <span><button val="zspage">正式</button></span>
@@ -102,197 +105,286 @@
         )
       }
 
-      function handler(type, target) {
+      function handler (type, target) {
         const uwpSystems = [
-          "bl-pms-front-end",
-          "bl-dap-front-end",
-          "bl-plm-front-end",
-          "bl-dms-front-end",
-          "bl-qms-front-end",
-          "bl-mms-front-end",
-          "bl-uwp-front-end",
+          'bl-pms-front-end',
+          'bl-dap-front-end',
+          'bl-plm-front-end',
+          'bl-dms-front-end',
+          'bl-qms-front-end',
+          'bl-mms-front-end',
+          'bl-uwp-front-end'
         ]
         const suwpSystems = [
-          "bl-suwp-front-end",
-          "bl-mps-front-end",
-          "bl-splm-front-end",
-          "bl-smms-front-end",
+          'bl-suwp-front-end',
+          'bl-mps-front-end',
+          'bl-splm-front-end',
+          'bl-smms-front-end'
         ]
 
-        const JenkinsBase = "https://jenkins.opsfun.com/job/"
-        const gitlabBlBase = "https://git.opsfun.com/bl_supply_chain/"
-        const gtilabBlBase02 = "https://git.opsfun.com/bl-backend/"
+        const JenkinsBase = 'https://jenkins.opsfun.com/job/'
+        const gitlabBlBase = 'https://git.opsfun.com/bl_supply_chain/'
+        const gtilabBlBase02 = 'https://git.opsfun.com/bl-backend/'
+        const gtilabBlBase03 = 'https://git.opsfun.com/blushmark-front/'
 
-        const buttonIptVal = $(target).attr("val")
-        const isGitlab = buttonIptVal.includes("gitlab")
-        const isProd = buttonIptVal.includes("prod")
-        const isPre = buttonIptVal.includes("pre")
-        const isPage = buttonIptVal.includes("page")
-        // const isTest = buttonIptVal.includes("test")
+        const buttonIptVal = $(target).attr('val')
+        const isGitlab = buttonIptVal.includes('gitlab')
+        const isProd = buttonIptVal.includes('prod')
+        const isPre = buttonIptVal.includes('pre')
+        const isPage = buttonIptVal.includes('page')
+        const isTest = buttonIptVal.includes('test')
 
-        var isNoSupportPreAndPage = isPre || isPage
+        // var isNoSupportPreAndPage = isPre || isPage
 
-        function searchSystem(systems, type) {
-          var system = systems.filter((item) => item.includes(`-${type}-`))
+        function searchSystem (systems, type) {
+          var system = systems.filter(item => item.includes(`-${type}-`))
           if (!system.length) {
-            console.log(`%c油猴插件提示：没有此系统${type}`, "color:#fd6327")
+            console.log(`%c油猴插件提示：没有此系统${type}`, 'color:#fd6327')
           }
           return system || []
         }
-        function jenkins(systems, systemType) {
-          if (isNoSupportPreAndPage) return alert("暂不支持此系统")
+        function jenkins (systems, systemType) {
+          if (isPre) return alert('暂不支持此系统')
 
-          var prod = buttonIptVal.includes("prod")
+          var prod = buttonIptVal.includes('prod')
           systems = systemType ? searchSystem(systems, systemType) : systems
           systems
-            .map((item) => {
+            .map(item => {
               return (
                 JenkinsBase +
                 item +
-                (prod ? "-prod" : "-test") +
-                "/build?delay=0sec"
+                (prod ? '-prod' : isTest ? '-test' : '-dev') +
+                '/build?delay=0sec'
               )
             })
-            .forEach((u) => window.open(u))
+            .forEach(u => window.open(u))
         }
-        function gitLab(systems, systemType) {
+        function gitLab (systems, systemType) {
           //return gitlabBlBase + item + '/compare/master...feature/qingyun/6666_layout'
           systems = systemType ? searchSystem(systems, systemType) : systems
           systems
-            .map((item) => gitlabBlBase + item + "/compare/master...master")
-            .forEach((u) => window.open(u))
+            .map(item => gitlabBlBase + item + '/compare/master...master')
+            .forEach(u => window.open(u))
         }
 
         // 获取riven商城的jenkins
-        function getRivenJkOrWh(clientType = "riven-pc") {
+        function getRivenJkOrWh (clientType = 'riven-pc') {
           // git仓库合并
           const gitMerge =
-            "https://git.opsfun.com/blushmark-front/riven-blush-mark/compare/develop...develop"
-
-          const envNum = $(target).parent().find("input").val() || 1
-          const yfbNum = envNum > 1 ? envNum : ""
+            gtilabBlBase03 + 'riven-blush-mark/compare/develop...develop'
+          const envNum =
+            $(target)
+              .parent()
+              .find('input')
+              .val() || 1
+          const yfbNum = envNum > 1 ? envNum : ''
           // 打印 2、3 环境的登录用户帐号
-          console.log("%cname: lebbay\npassword: passw0rd", "color: green")
+          console.log('%cname: lebbay\npassword: passw0rd', 'color: green')
 
           // jenkins
           const jenkins = {
-            "riven-pc": {
-              test: "https://j.opsfun.com/view/BM-FT/job/BL-riven-pc-test.dev/build?delay=0sec",
-              pre: "https://j.opsfun.com/view/BL-M/job/BL-PC-P-qa-prod.dev/build?delay=0sec",
-              prod: "https://j.opsfun.com/view/BL-PC/job/BL-PC-Switch-qa-prod.dev/build?delay=0sec",
+            'riven-pc': {
+              test:
+                'https://j.opsfun.com/view/BM-FT/job/BL-riven-pc-test.dev/build?delay=0sec',
+              pre:
+                'https://j.opsfun.com/view/BL-M/job/BL-PC-P-qa-prod.dev/build?delay=0sec',
+              prod:
+                'https://j.opsfun.com/view/BL-PC/job/BL-PC-Switch-qa-prod.dev/build?delay=0sec',
               cspage: `https://ft${envNum}-us.blushmark.com/`,
               yfbpage: `https://p${yfbNum}-us.blushmark.com/`,
-              zspage: `https://us.blushmark.com/`,
+              zspage: `https://us.blushmark.com/`
             },
-            "riven-m": {
-              test: "https://j.opsfun.com/view/BM-FT/job/BL-riven-m-test.dev/build?delay=0sec",
-              pre: "https://j.opsfun.com/view/BL-M/job/BL-M-P-qa-prod.dev/build?delay=0sec",
-              prod: "https://j.opsfun.com/view/BL-M/job/BL-M-Switch-qa-prod.dev/build?delay=0sec",
+            'riven-m': {
+              test:
+                'https://j.opsfun.com/view/BM-FT/job/BL-riven-m-test.dev/build?delay=0sec',
+              pre:
+                'https://j.opsfun.com/view/BL-M/job/BL-M-P-qa-prod.dev/build?delay=0sec',
+              prod:
+                'https://j.opsfun.com/view/BL-M/job/BL-M-Switch-qa-prod.dev/build?delay=0sec',
               cspage: `https://mt${envNum}.blushmark.com/us/`,
               yfbpage: `https://mp${yfbNum}.blushmark.com/us/`,
-              zspage: `https://m.blushmark.com/us/`,
-            },
+              zspage: `https://m.blushmark.com/us/`
+            }
           }
           const URL = isGitlab ? gitMerge : jenkins[clientType][buttonIptVal]
 
-          console.log(`%c跳转地址: ${URL}`, "color:#fd6327")
-
+          console.log(`%c跳转地址: ${URL}`, 'color:#fd6327')
           return URL
         }
+
+        function OpenUwpPageView (type) {
+          const Config = {
+            dap: {
+              cspage: 'https://bl-sc-dap-t-1.digi800.com/',
+              yfbpage: 'https://bl-dap-p.digi800.com/',
+              zspage: 'https://bl-dap.digi800.com/'
+            },
+            pms: {
+              cspage: 'http://mp-t-1.opsfun.com/web/pms/#/',
+              yfbpage: 'http://mp-p.opsfun.com/web/pms/#/',
+              zspage: 'https://mp.opsfun.com/web/pms/#/'
+            },
+            plm: {
+              kfpage: 'https://dev-bl-sc-plm.digi800.com/',
+              cspage: 'https://bl-sc-plm-t-1.digi800.com/',
+              yfbpage: '',
+              zspage: 'https://bl-plm.digi800.com/'
+            },
+            dms: {
+              kfpage: 'https://dev-bl-sc-front.digi800.com/',
+              cspage: 'https://bl-sc-front-t-1.digi800.com/',
+              yfbpage: 'https://bl-dms-p.digi800.com/',
+              zspage: 'https://bl-dms.digi800.com/'
+            },
+            qms: {
+              kfpage: 'https://dev-bl-sc-qms.digi800.com/',
+              cspage: 'https://bl-sc-qms-t-1.digi800.com/',
+              yfbpage: '',
+              zspage: 'https://bl-qms.digi800.com/'
+            },
+            mms: {
+              kfpage: 'https://dev-bl-sc-mms.digi800.com/',
+              cspage: 'https://bl-sc-mms-t-1.digi800.com/',
+              yfbpage: '',
+              zspage: 'https://bl-mms.digi800.com/'
+            },
+            uwp: {
+              kfpage: 'https://dev-bl-sc-uwp.digi800.com/',
+              cspage: 'https://bl-sc-uwp-t-1.digi800.com/',
+              yfbpage: 'https://bl-uwp-p.digi800.com/',
+              zspage: 'https://bl-uwp.digi800.com/'
+            }
+          }
+
+          const vUrl = Config[type][buttonIptVal]
+          vUrl ? window.open(vUrl) : alert('暂不支持')
+        }
+
         switch (type) {
-          case "uwp-a":
+          case 'uwp-a':
             isGitlab ? gitLab(uwpSystems) : jenkins(uwpSystems)
             break
-          case "suwp-a":
+          case 'suwp-a':
             isGitlab ? gitLab(suwpSystems) : jenkins(suwpSystems)
             break
-          case "pangu":
-            const pgjKbaseurl = "https://j.opsfun.com/view/BL-PG/job/"
-            const pgGitlabUrl =
-              gtilabBlBase02 + "pangu-web/compare/master...master"
-            const pgJenkinUrl = isProd
-              ? "BL-PG-WEB-qa-prod.dev/"
+          case 'cx':
+            const cxjKbaseurl = 'https://jenkins.opsfun.com/view/'
+            const cxGitlabUrl =
+              gtilabBlBase03 + 'promotion-dashboard/compare/develop...develop'
+            const cxJenkinUrl = isProd
+              ? 'bl-prod-us/job/promotion-dashboard-us-prod/'
               : isPre
-              ? "BL-PG-WEB-qa-pre.dev/"
-              : "BL-PG-WEB-new-test.dev/"
+              ? 'bl-pre-us/job/promotion-dashboard-us-pre/'
+              : 'bl-test-us/job/promotion-dashboard-us-test/'
+
+            const cxViewConf = {
+              cspage: 'http://mp-t-1.opsfun.com/web/promotion/#/',
+              yfbpage: 'http://mp-p.opsfun.com/web/promotion/#/',
+              zspage: 'https://mp.opsfun.com/web/promotion/#/'
+            }
+
+            if (isPage) {
+              const vUrl = cxViewConf[buttonIptVal]
+              vUrl ? window.open(vUrl) : alert('暂不支持')
+            } else {
+              window.open(isGitlab ? cxGitlabUrl : cxjKbaseurl + cxJenkinUrl)
+            }
+            break
+          case 'pangu':
+            const pgjKbaseurl = 'https://j.opsfun.com/view/BL-PG/job/'
+            const pgGitlabUrl =
+              gtilabBlBase02 + 'pangu-web/compare/master...master'
+            const pgJenkinUrl = isProd
+              ? 'BL-PG-WEB-qa-prod.dev/'
+              : isPre
+              ? 'BL-PG-WEB-qa-pre.dev/'
+              : 'BL-PG-WEB-new-test.dev/'
 
             const pgViewConf = {
-              cspage: "http://ft.bl-pangu.opsfun.com/#/dashboard",
-              yfbpage: "",
-              zspage: "https://pangu.opsfun.com/#/dashboard",
+              cspage: 'http://ft.bl-pangu.opsfun.com/#/dashboard',
+              yfbpage: '',
+              zspage: 'https://pangu.opsfun.com/#/dashboard'
             }
 
             if (isPage) {
               const vUrl = pgViewConf[buttonIptVal]
-              vUrl ? window.open(vUrl) : alert("暂不支持")
+              vUrl ? window.open(vUrl) : alert('暂不支持')
             } else {
               window.open(isGitlab ? pgGitlabUrl : pgjKbaseurl + pgJenkinUrl)
             }
             break
-          case "scm":
+          case 'scm':
             // scm有差异得专门处理
-            const gitlabUrl = gtilabBlBase02 + "scm/compare/master...master"
+            const gitlabUrl = gtilabBlBase02 + 'scm/compare/master...master'
             const jenkinUrl = isProd
-              ? "https://j.opsfun.com/job/BL-SCM-web-qa-prod.dev/build?delay=0sec"
-              : "https://j.opsfun.com/view/BL-SCM/job/BL-SCM-web-new-test.dev/build?delay=0sec"
+              ? 'https://j.opsfun.com/job/BL-SCM-web-qa-prod.dev/build?delay=0sec'
+              : 'https://j.opsfun.com/view/BL-SCM/job/BL-SCM-web-new-test.dev/build?delay=0sec'
 
             const scmViewConf = {
-              cspage: "https://blscm-test2.digi800.com/#/style",
-              yfbpage: "",
-              zspage: "https://blscm.digi800.com/#/style",
+              cspage: 'https://blscm-test2.digi800.com/#/style',
+              yfbpage: '',
+              zspage: 'https://blscm.digi800.com/#/style'
             }
 
             if (isPage) {
               const vUrl = scmViewConf[buttonIptVal]
-              vUrl ? window.open(vUrl) : alert("暂不支持")
+              vUrl ? window.open(vUrl) : alert('暂不支持')
             } else {
               window.open(isGitlab ? gitlabUrl : jenkinUrl)
             }
             break
-          case "riven-pc":
-          case "riven-m":
-          case "m":
-          case "pc":
-            type == "m" && (type = "riven-m")
-            type == "pc" && (type = "riven-pc")
+          case 'riven-pc':
+          case 'riven-m':
+          case 'm':
+          case 'pc':
+            type == 'm' && (type = 'riven-m')
+            type == 'pc' && (type = 'riven-pc')
             // blushmark 商城专门处理
-            window.open(getRivenJkOrWh(type))
+            const rvPageurl = getRivenJkOrWh(type)
+            !rvPageurl ? alert('暂不支持此项目') : window.open(rvPageurl)
             break
-          case "rv":
+          case 'rv':
             // blushmark 简写处理
-            window.open(getRivenJkOrWh("riven-pc"))
-            window.open(getRivenJkOrWh("riven-m"))
+            const rvpcPageurl = getRivenJkOrWh('riven-pc')
+            const rvmPageurl = getRivenJkOrWh('riven-m')
+            rvpcPageurl && window.open(rvpcPageurl)
+            rvmPageurl && window.open(rvmPageurl)
+            !rvpcPageurl && !rvmPageurl && alert('暂不支持此项目')
             break
           default:
-            const Sys = [...uwpSystems, ...suwpSystems]
-            isGitlab ? gitLab(Sys, type) : jenkins(Sys, type)
+            if (isPage) {
+              OpenUwpPageView(type)
+            } else {
+              const Sys = [...uwpSystems, ...suwpSystems]
+              isGitlab ? gitLab(Sys, type) : jenkins(Sys, type)
+            }
             break
         }
       }
-      $("body").append(getHtml())
-      $(".monkey-plugin-warp ul button").on("click", function () {
-        var inputs = ($(".monkey-plugin-input").val() || "uwp-a")
+      $('body').append(getHtml())
+      $('.monkey-plugin-warp ul button').on('click', function () {
+        var inputs = ($('.monkey-plugin-input').val() || 'uwp-a')
           .trim()
           .toLocaleLowerCase()
-          .split(" ")
-          .filter((v) => v)
-        inputs.forEach((type) => handler(type, this))
+          .split(' ')
+          .filter(v => v)
+        inputs.forEach(type => handler(type, this))
       })
-      $(".monkey-plugin-close").on("click", function () {
+      $('.monkey-plugin-close').on('click', function () {
         var self = $(this)
-        if (self.hasClass("monkey-plugin-entry")) {
-          $(".monkey-plugin-warp")
-            .removeClass("close")
-            .on("transitionend", function () {
-              localStorage.removeItem("monkey-plugin-hide")
-              self.removeClass("monkey-plugin-entry").text("X")
+        if (self.hasClass('monkey-plugin-entry')) {
+          $('.monkey-plugin-warp')
+            .removeClass('close')
+            .on('transitionend', function () {
+              localStorage.removeItem('monkey-plugin-hide')
+              self.removeClass('monkey-plugin-entry').text('X')
             })
         } else {
-          $(".monkey-plugin-warp")
-            .addClass("close")
-            .on("transitionend", function () {
-              localStorage.setItem("monkey-plugin-hide", 1)
-              self.addClass("monkey-plugin-entry").text("o")
+          $('.monkey-plugin-warp')
+            .addClass('close')
+            .on('transitionend', function () {
+              localStorage.setItem('monkey-plugin-hide', 1)
+              self.addClass('monkey-plugin-entry').text('o')
             })
         }
       })
