7db65a1128ec60ec11168dd1ec75b1fa98453a17
qingyun <qingyun@i9i8.com>
2022-10-18 17:02:37 +0800

    https

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MC方便B端系统发版的快捷小工具
-// @version      0.3.15
+// @version      0.3.15.1
 // @description  支持批量打开 blushMark商城和uwp和suwp子系统的jenkins与gitlab, 也支持空格批量打开多个单系统dap scm...的jenkins/gitlab
 // @author       mrzou
 // @match        https://bl-sc-pms-t-1.digi800.com/#/index
@@ -208,11 +208,11 @@
             },
             "riven-m-d": {
               test: "https://jenkins.opsfun.com/view/DM-FT/job/DM-riven-m-test/build?delay=0sec",
-              cspage: `http://mt${envNum}.dolmi.com/us`,
+              cspage: `https://mt${envNum}.dolmi.com/us`,
             },
             "riven-pc-d": {
               test: "https://jenkins.opsfun.com/view/DM-FT/job/DM-riven-pc-test/build?delay=0sec",
-              cspage: `http://ft${envNum}-us.dolmi.com/`,
+              cspage: `https://ft${envNum}-us.dolmi.com/`,
             },
           }
           const URL = isGitlab ? gitMerge : jenkins[clientType][buttonIptVal]
